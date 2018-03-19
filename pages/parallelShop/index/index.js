57b187355caa123afae4178b9310406d68684c0c
jing <15737951003@163.com>
2018-03-19 09:07:22 +0800

    店内详情页js功能完善

---
+++ pages/parallelShop/index/index.js
@@ -0,0 +1,24 @@
+Page({
+  data: {
+    imgUrls: [
+      '../../../images/199.jpg',
+      '../../../images/makers.jpg',
+      '../../../images/pingxingdian.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    circular: true,
+    iconGroup: [
+      { img: '../../../images/allocated.png', text: '待领配', url: "./waitReceive/waitReceive" },
+      { img: '../../../images/assign.png', text: '已领配' },
+      { img: '../../../images/evaluated.png', text: '待评价' },
+    ],
+    parallel: [
+      { img: '../../../images/capital-1.jpg', title: '科技奶茶', busin: '超市', addr: '深圳市南山区TCL国际E城', url: "../shopDetail/shopDetail" },
+      { img: '../../../images/capital-1.jpg', title: '北京汇雅美苑文化创意发展有限公司', busin: '超市', addr: '深圳市南山区TCL国际E城', url: "../shopDetail/shopDetail" },
+      { img: '../../../images/capital-1.jpg', title: '北京红日升商贸有限公司', busin: '超市', addr: '深圳市南山区TCL国际E城', url: "../shopDetail/shopDetail" },
+    ],
+  },
+})
