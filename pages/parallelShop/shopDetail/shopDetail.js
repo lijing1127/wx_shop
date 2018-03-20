5d84e3547782f45b92f9166d3ddb6885807d5c53
jing <15737951003@163.com>
2018-03-20 15:25:43 +0800

    店内详情页js判断领配额和产品金额功能等

--- pages/parallelShop/shopDetail/shopDetail.js
+++ pages/parallelShop/shopDetail/shopDetail.js
@@ -7,9 +7,12 @@ Page({
       // { img: '../../images/capital-1.jpg', title: '北京汇雅美苑文化创意发展有限公司', busin: '超市', addr: '深圳市南山区TCL国际E城' },
       // { img: '../../images/capital-1.jpg', title: '北京红日升商贸有限公司', busin: '超市', addr: '深圳市南山区TCL国际E城' },
     ],
-    shopProduct: [],
-    totalPrice: 0,
-    showModal: false,
+    shopProduct: [],//产品列表
+    showModal: false,//是否展示模态框
+    quota:1000,//领配额
+    totalPrice: 0,//选择领配产品的总额
+    backgroundColor: '#d9d9d9',
+    showText:"请选择",
   },
   onShow(){
     this.setData({
@@ -20,6 +23,7 @@ Page({
       ]
     });
   },
+  //计算总额
   getTotalPrice(){
     let shopProduct = this.data.shopProduct;                  // 获取购物车列表
     let total = 0;
@@ -31,19 +35,20 @@ Page({
       totalPrice: total.toFixed(2)
     });
   },
+  //增加数量
   addClick: function(e){
     const index = e.currentTarget.dataset.index;
-    console.log(index);
     let shopProduct = this.data.shopProduct; 
     let num = parseInt(shopProduct[index].num);
     num = num + 1;
-    shopProduct[index].num = num;
+    shopProduct[index].num = num;  
     this.setData({
-      shopProduct: shopProduct
+      shopProduct: shopProduct,
     });
     this.getTotalPrice();
-    
+    this.comparAmount();
   },
+  //减少数量
   cutClick: function(e){
     const index = e.currentTarget.dataset.index;
     let shopProduct = this.data.shopProduct;
@@ -57,7 +62,10 @@ Page({
       shopProduct: shopProduct
     });
     this.getTotalPrice();
+    this.comparAmount();
   },
+
+  //模态框点击展示功能
   preventTouchMove: function () {
   },
 
@@ -66,9 +74,33 @@ Page({
       showModal:true
     });
   },
+
   hideDetail(){
     this.setData({
       showModal: false
     });
-  }
+  },
+
+  //比较领配额和选中产品的价格
+  comparAmount(){
+    let quota = this.data.quota;
+    let totalPrice = Number(this.data.totalPrice);
+    this.getTotalPrice();
+    if (quota < totalPrice) {
+      this.setData({
+        backgroundColor:'#d9d9d9',
+        showText:"超出额度"
+      })
+    } else if (totalPrice == 0){
+      this.setData({
+        backgroundColor: '#d9d9d9',
+        showText: "请选择"
+      })
+    }else{
+      this.setData({
+        backgroundColor: '#ad0e11',
+        showText: "平行领配"
+      })
+    }
+  },
 })
