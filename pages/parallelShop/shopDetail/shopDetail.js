aa7efb941386fac3187405c543dd051c06096072
jing <15737951003@163.com>
2018-04-24 10:41:41 +0800

    店铺详情页请求后台数据渲染页面

--- pages/parallelShop/shopDetail/shopDetail.js
+++ pages/parallelShop/shopDetail/shopDetail.js
@@ -2,33 +2,22 @@
 Page({
   /** * 页面的初始数据 */
   data: {
-    shopData: [
-      { img: '../../../images/capital-1.jpg', title: '科技奶茶', busin: '超市', addr: '深圳市南山区TCL国际E城' },
-      // { img: '../../images/capital-1.jpg', title: '北京汇雅美苑文化创意发展有限公司', busin: '超市', addr: '深圳市南山区TCL国际E城' },
-      // { img: '../../images/capital-1.jpg', title: '北京红日升商贸有限公司', busin: '超市', addr: '深圳市南山区TCL国际E城' },
-    ],
+    shopData: [],//店铺信息
     shopProduct: [],//产品列表
     showModal: false,//是否展示模态框
     quota:1000,//领配额
     totalPrice: 0,//选择领配产品的总额
     backgroundColor: '#d9d9d9',
     showText:"请选择",
+    go:false
   },
-  onShow(){
-    this.setData({
-      shopProduct: [
-        { img: '../../../images/L-alabo.png', name: '阿拉伯糖', price: '980.0', num: '0' },
-        { img: '../../../images/L-alabo.png', name: '阿拉伯糖', price: '980.0', num: '0' },
-        { img: '../../../images/L-alabo.png', name: '阿拉伯糖', price: '980.0', num: '0' },
-      ]
-    });
-  },
+
   //计算总额
   getTotalPrice(){
     let shopProduct = this.data.shopProduct;                  // 获取购物车列表
     let total = 0;
     for (let i = 0; i < shopProduct.length; i++) {         // 循环列表得到每个数据// 判断选中才会计算价格
-      total += shopProduct[i].num * shopProduct[i].price;     // 所有价格加起来  
+      total += shopProduct[i].num * shopProduct[i].now_product_price;     // 所有价格加起来  
     }
     this.setData({                                // 最后赋值到data中渲染到页面
       shopProduct: shopProduct,
@@ -81,7 +70,7 @@ Page({
     });
   },
 
-  //比较领配额和选中产品的价格
+  //比较领配额和选中产品的价格的大小出现不同的状态
   comparAmount(){
     let quota = this.data.quota;
     let totalPrice = Number(this.data.totalPrice);
@@ -89,7 +78,7 @@ Page({
     if (quota < totalPrice) {
       this.setData({
         backgroundColor:'#d9d9d9',
-        showText:"超出额度"
+        showText:"超出额度",
       })
     } else if (totalPrice == 0){
       this.setData({
@@ -99,8 +88,41 @@ Page({
     }else{
       this.setData({
         backgroundColor: '#ad0e11',
-        showText: "平行领配"
+        showText: "平行领配",
+        go:true
       })
+      
     }
   },
+
+  onLoad:function(options){
+    var shopId = options.id;
+    console.log(shopId)
+    var that = this;
+    wx.request({
+      url: 'http://192.168.1.235:3000/parallel_shop/shopdata_products?id='+shopId,
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        var arr = [];
+        var res_obj;
+        var datas = res.data.product_info;
+        for (var i = 0; i < datas.length; i++) {
+          res_obj = Object.assign({}, res.data.product_info[i], {num: 0});
+          arr.push(res_obj);
+        }
+        that.setData({ shopData: res.data, shopProduct: arr });            
+      }
+    })     
+  },
+
+  //平行领配跳转页面
+  receiveDetail: function() {
+    if (this.go){
+      wx.navigateTo({
+        url: '../receiveDetail/receiveDetail',
+      })
+    }
+  }
 })
